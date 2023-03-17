515c043752a609cb3cba6d71f650c0dc74d8cfea
SirJimKe <jimmymutuku1@gmail.com>
2023-03-17 02:28:43 +0000

    ✨ Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a statement using the funtion printf
+ *
+ * Return: 0 if the program exits successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
