6c52ee8844c8762688a26ae53d78c52f1f845659
SirJimKe <jimmymutuku1@gmail.com>
2023-03-17 02:18:36 +0000

    ✨ Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a statement using the funtion puts
+ *
+ * Return: 0 is exited successfully
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
