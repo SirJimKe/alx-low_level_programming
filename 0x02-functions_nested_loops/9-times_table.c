03567128559f6c55fbcb92d70aa08853ebb5206d
SirJimKe <jimmymutuku1@gmail.com>
2023-03-22 17:30:46 +0000

    🚑️ hot fix multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,6 +2,8 @@
 
 /**
  * times_table - prints the 9 times table
+ *
+ * nested loops
  * starts with 0
  * Return: multiples
  */
@@ -17,17 +19,15 @@ void times_table(void)
 		{
 			int r = j * i;
 
+			_putchar(',');
+			_putchar(' ');
 			if (r < 10)
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 				_putchar(r + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
 				_putchar(r / 10 + '0');
 				_putchar(r % 10 + '0');
 			}
