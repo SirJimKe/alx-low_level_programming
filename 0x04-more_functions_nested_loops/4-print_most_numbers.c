fa4d599d95c3715665baa3629237c47746e8543d
SirJimKe <jimmymutuku1@gmail.com>
2023-03-23 16:53:21 +0000

    ✨ print numbers 01356789

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numvers 0-9 except 2 and 4
+ * Return: 01356789
+ */
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+
+		i++;
+	}
+
+	_putchar('\n');
+}
