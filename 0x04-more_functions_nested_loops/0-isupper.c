655955014bb63eaac9f5abd87a5ed854b3075c21
SirJimKe <jimmymutuku1@gmail.com>
2023-03-23 15:24:05 +0000

    check uppacase characters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: parameter
+ * Return: 1 if c is uppercase and 0 if otherwise
+ */
+int _isupper(int c)
+{
+	return (isupper(c));
+}
