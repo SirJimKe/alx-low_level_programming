5d34d6496b0b58e78a7e6a6076b9482f8f8c7433
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-03-25 00:55:26 +0000

    🚑️ hot fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,13 +9,18 @@ void print_triangle(int size)
 {
 	int i, j, k;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = size - i; j > 1; j--)
-			_putchar(' ');
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - i; j > 1; j--)
+				_putchar(' ');
 
-		for (k = 0; k <= i; k++)
-			_putchar('#');
-		_putchar('\n');
+			for (k = 0; k <= i; k++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
+	else
+		_putchar('\n');
 }
