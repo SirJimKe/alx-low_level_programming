7976745082bfaa919ca946b9b03f6cbba7d42b2c
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-03-28 00:34:00 +0000

    ✨ prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a new line
+ * @str: parameter
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
