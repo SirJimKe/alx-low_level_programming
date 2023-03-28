dd9edc2d69c293067f534bb4a4d7350e197fb0a6
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-03-28 02:33:53 +0000

    ✨ prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: paramenter
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i, n, len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	n = len / 2;
+
+	if (len % 2 == 1)
+		n = (len - 1) / 2;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	putchar('\n');
+}
