19c46a513eb6d55caf5ebd235663f3ee53fdeb1b
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-03-30 16:32:30 +0000

    ✨ copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies another string
+ * @dest: where strings will be copied
+ * @src: string to be copied
+ * @n: number of bytes
+ * Return: result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && *src != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
