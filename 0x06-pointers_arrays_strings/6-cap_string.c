bb422c09e2fb471a94f82f9b0a5d87936fdce097
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-03-31 02:42:57 +0000

    hot fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,29 +8,26 @@
 char *cap_string(char *s)
 {
 	int i = 0;
+	int j;
+	char sep[] = { ' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(',
+		       ')', '{', '}' };
 
-	/**
-	 * iterate through the string
-	 * find small letters
-	 * if first letter is small capitalize
-	 * find separators of words
-	 * if a letter following a separator is lower, capitalize
-	 */
+	if (s[i] >= 'a' && s[i] <= 'z')
+		s[i] -= 32;
+
 	while (s[i] != '\0')
 	{
-		if (s[i] > 96 && s[i] < 123)
-		{
-			if (i == 0)
-				s[i] -= 32;
+		if (s[i] == '\t')
+			s[i] = ' ';
 
-			if (s[i - 1] == 32 || s[i - 1] == 9 ||
-			    s[i - 1] == 10 || s[i - 1] == 44 ||
-			    s[i - 1] == 59 || s[i - 1] == 33 ||
-			    s[i - 1] == 63 || s[i - 1] == 34 ||
-			    s[i - 1] == 40 || s[i - 1] == 41 ||
-			    s[i - 1] == 123 || s[i - 1] == 125 ||
-				s[i - i] == 46)
-				s[i] -= 32;
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
+				break;
+			}
 		}
 		i++;
 	}
