01d80f90d1c43d782001c38c71715edc88396a80
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-04-03 15:15:23 +0000

    🐛 fixed output

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,10 +6,21 @@
  */
 char *_strchr(char *s, char c)
 {
+	/**
+	 * iterate s to find if c exists
+	 * if c exists return the pointer of c
+	 */
 	for (; *s != '\0'; s++)
 	{
 		if (*s == c)
 			return (s);
 	}
+	/**
+	 * if c is '\0'
+	 * return the pointer to s
+	 */
+	if (*s == c)
+		return (s);
+
 	return (0);
 }
