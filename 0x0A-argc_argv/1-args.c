483482d4ba7dea3c576aebe4115127fd6401cbc8
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-04-10 16:15:10 +0000

    ✨✨ print argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 * File: 0-whatsmyname.c is a program that prints its name, followed by a new line.
+* File: 1-args.c prints the number of arguments passed in a function
