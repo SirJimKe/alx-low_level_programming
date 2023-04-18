3f0e7d0f2db9a11a99da25099dcfe64d541c00b8
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-04-18 01:08:48 +0000

    🚑️🚑️ prints a dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,11 +16,11 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: %s\n", d->name);
 		if (d->age < 0)
-			printf("Age: (nil)");
+			printf("Age: (nil)\n");
 		else
 			printf("Age: %1f\n", d->age);
 		if (d->owner == NULL)
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
 	}
