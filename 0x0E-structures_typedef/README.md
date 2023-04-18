99d1efbb848abfd22af7031c25e8e2e752ec6d54
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-04-18 00:22:24 +0000

    ✨✨ struct dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+* File: dog.h is a struct dog
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type describing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Description: a new type that consistes of dog details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
