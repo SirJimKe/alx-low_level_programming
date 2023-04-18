71025bd911c3b7802a2d3cfd02c1586a18056079
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-04-18 02:15:55 +0000

    ✨✨ created new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,65 @@
+#include "dog.h"
+#include <stdlib.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * _strlen - finds string length
+ * @s: pointer to string input
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * _strcpy - copies two strings
+ * @dest: original string
+ * @src: string to be a copy
+ * Return: copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: pointer to dog name
+ * @age: age of the dog
+ * @owner: pointer to dog owner
+ * Return: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newDog;
+
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
+
+	newDog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	newDog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+
+	if (newDog->name == NULL || newDog->owner == NULL)
+		return (NULL);
+
+	_strcpy(newDog->name, name);
+	_strcpy(newDog->owner, owner);
+
+	newDog->age = age;
+
+	return (newDog);
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,4 @@
 * File: dog.h is a struct dog
 * File: 1-init_dog.c initializes a variable of type struct dog
 * File: 2-print_dog.c prints a struct dog
+* File: 4-new_dog.c creates a new dog
