f6aea82efdd1e3c1bddce1498c4d63917be19287
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-04-20 02:56:59 +0000

    ✨✨ operators

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return remainder after division of two integers
+ * @a: first int
+ * @b: second int
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
