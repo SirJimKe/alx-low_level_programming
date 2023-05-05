524661468646c46232d84b36fec3f831c9d6f87a
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-05-05 02:22:01 +0000

    ✨✨ clear bits

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: interger input value
+ * @index: index starting from 0
+ * Return: 1 or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n &= ~mask;
+
+	return (1);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -2,3 +2,4 @@
 * File: 1-print_binary.c is a function that prints the binary representation of a number.
 * File: 2-get_bit.c returns the value of a bit at a given index
 * File: 3-set_bit.c sets the value of a bit to 1 at a given index.
+* File: 4-clear_bit.c sets the value of a bit to 0 at a given index
