26c1dc83c0131e66598617868161162ccce81b84
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-05-09 00:36:18 +0000

    ✨✨ read and print a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters to be read and printed
+ * Return: number of letters or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *file;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+		return (0);
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		fclose(file);
+		return (0);
+	}
+
+	bytes_read = fread(buffer, sizeof(char), letters, file);
+	if (bytes_read == 0)
+	{
+		free(buffer);
+		fclose(file);
+		return (0);
+	}
+
+	bytes_written = fwrite(buffer, sizeof(char), bytes_read, stdout);
+	free(buffer);
+	fclose(file);
+
+	if (bytes_written != bytes_read)
+		return (0);
+
+	return (bytes_written);
+}
